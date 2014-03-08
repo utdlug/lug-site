cb1d9396c65d0493f4e022c9374004730ffe23c8
Mark Neves <mneves@gmail.com>
2014-03-07 18:56:24 -0600

    Added stat routing

--- app.js
+++ app.js
@@ -5,10 +5,10 @@ var express = require('express')
 var fs = require('fs');
 var app = express();
 var appRedirect = express();
-
+var certPath = "/var/www/certs/";
 var options = {
-    key: fs.readFileSync('/var/www/certs/lug.utdallas.edu.key'),
-    cert: fs.readFileSync('/var/www/certs/lug.utdallas.edu.cer')
+    key: fs.readFileSync(certPath + 'lug.utdallas.edu.key'),
+    cert: fs.readFileSync(certPath + 'lug.utdallas.edu.cer')
 //   rejectUnauthorized: false
 };
 
@@ -53,36 +53,19 @@ if ('development' == app.get('env')) {
 
 // Routing
 app.get('/', function(req, res) {
-    if(!req.secure) {
-        res.redirect(301, "https://lug.utdallas.edu");
-    }
-    else {
         res.render('index', {title: "Linux Users Group @ UTD"});
-    }
 });
 app.get('/join', function(req, res) {
-    if(!req.secure) {
-        res.redirect(301, "https://" + req.headers.host + req.url);
-    }
-    else {
         res.redirect("https://orgsync.com/join/15316/linux-users-group");
-    }
 });
 app.get('/chat', function(req, res) {
-    if(!req.secure) {
-        res.redirect(301, "https://" + req.headers.host + req.url);
-    }
-    else {
         res.redirect("https://lug.utdallas.edu:3000");
-    }
 });
 app.get('/irc', function(req, res) {
-    if(!req.secure) {
-        res.redirect(301, "https://" + req.headers.host + req.url);
-    }
-    else {
         res.redirect("https://lug.utdallas.edu:3000");
-    }
+});
+app.get('/stats', function(req, res) {
+        res.redirect("http://stats.utdlug.org.s3-website-us-east-1.amazonaws.com/");
 });
 
 http.createServer(appRedirect).listen(appRedirect.get('port'), function(){
@@ -92,4 +75,3 @@ http.createServer(appRedirect).listen(appRedirect.get('port'), function(){
 https.createServer(options,app).listen(app.get('port'), function(){
   console.log('Express server (SSL) listening on port ' + app.get('port'));
 });
-
