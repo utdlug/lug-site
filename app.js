29da021354a1ef0690c7bd33934282dedcc98050
Mark Neves <mneves@gmail.com>
2014-03-20 19:47:21 -0500

    Tidied up code + updated readme

--- app.js
+++ app.js
@@ -1,42 +1,33 @@
-var express = require('express')
-  , http = require('http') , https = require('https') , path = require('path');
-var fs = require('fs');
-var app = express();
-var appRedirect = express();
+var express = require('express'), fs = require('fs'), http = require('http'),
+  https = require('https'), path = require('path');
+var app = express(), appRedirect = express();
 var siteUrl = "https://lug.utdallas.edu";
 var certPath = "/var/www/certs/";
 var options = {
-    key: fs.readFileSync(certPath + 'lug.utdallas.edu.key'),
-    cert: fs.readFileSync(certPath + 'lug.utdallas.edu.cert.cer'),
-    ca: [ 
+  key: fs.readFileSync(certPath + 'lug.utdallas.edu.key'),
+  cert: fs.readFileSync(certPath + 'lug.utdallas.edu.cert.cer'),
+  ca: [
         fs.readFileSync(certPath + 'root.cer'),
         fs.readFileSync(certPath + 'inter1.cer')
-        ]
+      ]
 };
 var Subway = require('./irc/lib/subway');
 var subway = new Subway();
 
-app.use(app.router);
-app.use(express.static(path.join(__dirname, 'public')));
 app.set('views', __dirname + '/views');
 app.set('port', process.env.PORT || 3001);  // dev
 app.set('view engine', 'jade');
+app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.logger('dev'));
 app.use(express.bodyParser());
 app.use(express.methodOverride());
-
-appRedirect.set('port', 3002);
-appRedirect.get("*", function (req, res) {
-   res.redirect(siteUrl + req.path);
-});
-
-app.use(function errorHandler(err, req, res, next) {
+app.use(function(err, req, res, next) {
   var code = err.code;
   var message = err.message;
   res.writeHead(code, message, {'content-type' : 'text/plain'});
   res.end(message);
 });
-
 app.use(function(req, res, next){
   res.status(404);
   res.redirect(siteUrl);
@@ -46,21 +37,25 @@ if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
-// Routing
+appRedirect.set('port', 3002);
+appRedirect.get("*", function (req, res) {
+   res.redirect(siteUrl + req.path);
+});
+
 app.get('/', function(req, res) {
-        res.render('index', {title: "Linux Users Group @ UTD"});
+  res.render('index', {title: "Linux Users Group @ UTD"});
 });
 app.get('/join', function(req, res) {
-        res.redirect("https://orgsync.com/join/15316/linux-users-group");
+  res.redirect("https://orgsync.com/join/15316/linux-users-group");
 });
 app.get('/chat', function(req, res) {
-        res.redirect(siteUrl + ":3000");
+  res.redirect(siteUrl + ":3000");
 });
 app.get('/irc', function(req, res) {
-        res.redirect(siteUrl + ":3000");
+  res.redirect(siteUrl + ":3000");
 });
 app.get('/stats', function(req, res) {
-        res.redirect("http://stats.utdlug.org.s3-website-us-east-1.amazonaws.com/");
+  res.redirect("http://stats.utdlug.org.s3-website-us-east-1.amazonaws.com/");
 });
 
 http.createServer(appRedirect).listen(appRedirect.get('port'), function(){
